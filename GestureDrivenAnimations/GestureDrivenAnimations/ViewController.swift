1a042bee3a52959966a402cfe8be5ba3cd16e49d
khou22 <kevin.ch.hou@gmail.com>
2016-06-27 16:18:05 -0700

    Interpolate Library Working

--- GestureDrivenAnimations/GestureDrivenAnimations/ViewController.swift
+++ GestureDrivenAnimations/GestureDrivenAnimations/ViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 import Interpolate
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    var opacityChange: Interpolate?
+    var logoPosition: Interpolate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(ViewController.didPan(_:)))
+        view.addGestureRecognizer(pan)
+        
+        opacityChange = Interpolate(
+            from: 1, to: 0, apply: { [weak self] (opacity) in
+                self?.logoImage.alpha = opacity
+            })
+        logoPosition = Interpolate(from: 0, to: view.bounds.size.width, apply: { [weak self] (xPosition) in
+            self?.logoImage.layer.position.x = xPosition
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +41,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func didPan(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view)
+        let translatedCenterX = translation.x
+        let progress = translatedCenterX / 200
+//        print(progress)
+        opacityChange?.progress = progress
+        logoPosition?.progress = progress
+    }
 
 }
 
