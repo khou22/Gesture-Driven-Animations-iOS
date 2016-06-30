35150901518e847655939aa8c4a38a674b72c029
khou22 <kevin.ch.hou@gmail.com>
2016-06-30 15:14:14 -0700

    Implemented scroll-based animations
    Using UIViewPropertyAnimator

--- GestureDrivenAnimations/GestureDrivenAnimations/Pager.swift
+++ GestureDrivenAnimations/GestureDrivenAnimations/Pager.swift
@@ -9,7 +9,14 @@
 import Foundation
 import UIKit
 
+struct PercentageScrolled {
+    // Global variable for percentage scrolled
+    static var value: Float = 0.0
+}
+
 class Pager: UIPageViewController {
+    
+    var colorChange: UIViewPropertyAnimator?
     
     func getPageOne() -> PageOne {
         return storyboard!.instantiateViewController(withIdentifier: "PageOne") as! PageOne
@@ -21,12 +28,24 @@ class Pager: UIPageViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         print("Pager appeared")
-        
-        setViewControllers([getPageOne()], direction: .forward, animated: false, completion: nil)
-        
+        setViewControllers([getPageOne()], direction: .forward, animated: false, completion: nil) // Set first page
         dataSource = self
         
-        view.backgroundColor = UIColor(colorLiteralRed: 1, green: 1, blue: 1, alpha: 1)
+        // Set animations
+        self.view.backgroundColor = UIColor.black()
+        colorChange = UIViewPropertyAnimator(duration: 0.3, curve: .easeIn, animations: { [weak self] in
+            self?.view.backgroundColor = UIColor(red: 255.0/255.0, green: 80.0/255.0, blue: 43.0/255.0, alpha: 1.0)
+            })
+    }
+    
+    override func viewDidLoad() {
+        // Scrolling progress - from: http://stackoverflow.com/questions/22577929/progress-of-uipageviewcontroller
+        super.viewDidLoad()
+        for subView in view.subviews {
+            if subView is UIScrollView {
+                (subView as! UIScrollView).delegate = self
+            }
+        }
     }
     
 }
@@ -54,5 +73,32 @@ extension Pager: UIPageViewControllerDataSource {
             // End of all pages
             return nil
         }
+    }
+    
+    // ********* Sets up the page control dots *********
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return 2
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        // On the first dot when you first load the Pager
+        // Swift automatically handles switching pages and updating the page control dots
+        // Updates when setViewControllers is called
+        return 0
+    }
+}
+
+extension Pager: UIScrollViewDelegate {
+    
+    // Track the progress of the scroll between pages
+    // http://stackoverflow.com/questions/22577929/progress-of-uipageviewcontroller
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let point = scrollView.contentOffset
+        var percentComplete: CGFloat
+        percentComplete = fabs(point.x - view.frame.size.width)/view.frame.size.width // Calc percentage complete
+        print("Percent of Scroll Completed: \(percentComplete)") // Feedback
+        PercentageScrolled.value = Float(percentComplete) // Update the value
+        colorChange?.fractionComplete = percentComplete
     }
 }
