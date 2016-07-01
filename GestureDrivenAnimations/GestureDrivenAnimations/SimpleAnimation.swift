7f88cdbc51bb796b9b24238f2b5ec8e047fcdb64
khou22 <kevin.ch.hou@gmail.com>
2016-07-01 10:52:48 -0700

    Added new page

---
+++ GestureDrivenAnimations/GestureDrivenAnimations/SimpleAnimation.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleAnimation.swift
+//  GestureDrivenAnimations
+//
+//  Created by Breathometer on 7/1/16.
+//  Copyright © 2016 KevinHou. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SimpleAnimation: UIViewController {
+    override func viewDidLoad() {
+        print("Simple animation screen loaded")
+    }
+}
